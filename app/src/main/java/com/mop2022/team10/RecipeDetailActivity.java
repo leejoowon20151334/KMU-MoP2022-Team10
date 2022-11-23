3f92e645092db7665b36b51562b73b2a280f7d95
leejoowon20151334 <jwlee51@kookmin.ac.kr>
2022-11-24 02:14:30 +0900

    레시피 내용 페이지 생성

--- app/src/main/java/com/mop2022/team10/RecipeDetailActivity.java
+++ app/src/main/java/com/mop2022/team10/RecipeDetailActivity.java
@@ -9,6 +9,7 @@ import androidx.core.content.FileProvider;
 import android.Manifest;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -20,10 +21,13 @@ import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.mop2022.team10.Rest.Ingredient;
@@ -42,76 +46,167 @@ import java.util.ArrayList;
 
 public class RecipeDetailActivity extends AppCompatActivity {
 
-    private TextView test;
-    private Button testBtn,testCamera;
-    private ImageView testImg;
-    private String imageFilePath;
-    private Uri photoUri;
+    private Button cameraBtn;
+
+    private int recipeId;
+    private TextView titleText;
+    private ImageView recipeImg;
+    private TextView description;
+    private TextView recipeTime;
+    private TextView difficulty;
+    private LinearLayout ingredients;
+    private LinearLayout procedures;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
-        test = (TextView) findViewById(R.id.recipeDetail_test);
-        testBtn = (Button) findViewById(R.id.recipeDetail_testBtn);
-        testImg = (ImageView) findViewById(R.id.recipeDetail_testImg);
-        testCamera = (Button) findViewById(R.id.recipeDetail_testCamera);
+
+        if(getIntent().hasExtra("recipeId"))
+            recipeId = (int) getIntent().getExtras().get("recipeId");
+        else
+            recipeId = 1;
+
+        titleText = findViewById(R.id.recipeDetail_titleText);
+        recipeImg = findViewById(R.id.recipeDetail_recipeImg);
+        description = findViewById(R.id.recipeDetail_description);
+        recipeTime = findViewById(R.id.recipeDetail_recipeTime);
+        difficulty = findViewById(R.id.recipeDetail_difficulty);
+        ingredients = findViewById(R.id.recipeDetail_ingredients);
+        procedures = findViewById(R.id.recipeDetail_procedures);
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Recipe recipe = new Recipe();
+                RecipeModel result = recipe.getRecipeDetail(recipeId);
+                if(result.id!=recipeId)
+                    finish();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(result.name!=null)
+                            titleText.setText(result.name);
+                        if(result.img!=null)
+                            recipeImg.setImageBitmap(recipe.getImg(result.img));
+                        if(result.description!=null)
+                            description.setText(result.description);
+                        if(result.time > 0)
+                            recipeTime.setText(Integer.toString(result.time) + "분");
+                        if(result.difficulty > 0){
+                            if(result.difficulty < 3)
+                                difficulty.setText("쉬워요");
+                            else if(result.difficulty < 5)
+                                difficulty.setText("약간 쉬워요");
+                            else if(result.difficulty < 7)
+                                difficulty.setText("할만해요");
+                            else if(result.difficulty < 9)
+                                difficulty.setText("조금 어려워요");
+                            else
+                                difficulty.setText("어려워요");
+                        }
+
+                        Context context = getApplicationContext();
+
+                        for(IngredientModel ingredient : result.ingredients){
+
+                            LinearLayout ingredientLine = new LinearLayout(context);
+                            ingredientLine.setLayoutParams(new ViewGroup.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,150));
+
+                            //식자재 이미지
+                            ImageView ingredientImg = new ImageView(context);
+                            LinearLayout.LayoutParams imgParam = new LinearLayout.LayoutParams(0,LinearLayout.LayoutParams.MATCH_PARENT);
+                            imgParam.weight = 1f;
+                            ingredientImg.setLayoutParams(imgParam);
+                            if(ingredient.img!=null)
+                                ingredientImg.setImageBitmap(recipe.getImg(ingredient.img));
+                            ingredientLine.addView(ingredientImg);
+
+                            //식자재 이름
+                            TextView ingredientName = new TextView(context);
+                            LinearLayout.LayoutParams nameParam = new LinearLayout.LayoutParams(0,LinearLayout.LayoutParams.MATCH_PARENT );
+                            nameParam.weight = 2f;
+                            ingredientName.setLayoutParams(nameParam);
+                            ingredientName.setTextSize(25);
+                            ingredientName.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                            ingredientName.setGravity(Gravity.CENTER);
+                            ingredientName.setText(ingredient.name);
+                            ingredientLine.addView(ingredientName);
+
+                            //식자재 갯수
+                            TextView ingredientCount = new TextView(context);
+                            LinearLayout.LayoutParams countParam = new LinearLayout.LayoutParams(0,LinearLayout.LayoutParams.MATCH_PARENT );
+                            countParam.weight = 1f;
+                            ingredientCount.setLayoutParams(countParam);
+                            ingredientCount.setTextSize(25);
+                            ingredientCount.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                            ingredientCount.setGravity(Gravity.CENTER);
+                            ingredientCount.setText(Double.toString(ingredient.count));
+                            ingredientLine.addView(ingredientCount);
+
+                            LinearLayout margin = new LinearLayout(context);
+                            margin.setLayoutParams(new ViewGroup.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,50));
+
+                            ingredients.addView(ingredientLine);
+                            ingredients.addView(margin);
+                        }
+
+                        int i=1;
+                        for(String procedureStr : result.procedure){
+                            TextView procedure = new TextView(context);
+                            procedure.setTextSize(15);
+                            procedure.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+                            procedure.setGravity(Gravity.CENTER);
+                            procedure.setText(Integer.toString(i) + ". "+procedureStr);
+                            procedures.addView(procedure);
+
+                            LinearLayout margin = new LinearLayout(context);
+                            margin.setLayoutParams(new ViewGroup.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,50));
+                            procedures.addView(margin);
+                            i++;
+                        }
+
+
+                    }
+                });
+            }
+        });
+        t.start();
+    }
+
+    /*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        //기타 로직
+
+
 
         String[] permissions = {
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
         };
 
+        //카메라 호출 버튼
+        cameraBtn = (Button) findViewById(R.id.recipeDetail_testCamera);
+
+        //촬영한 사진 관련 permission check
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
         if(permissionCheck != PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(this, permissions, 1);
 
-        testCamera.setOnClickListener(new View.OnClickListener(){
+        //카메라 호출 버튼 클릭시 작동
+        cameraBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 sendTakePhotoIntent();
             }
         });
-
-        testBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Thread t = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Recipe recipe = new Recipe();
-                        RecipeModel result = recipe.getRecipeDetail(1);
-                        Bitmap img = recipe.getImg(result.img);
-
-                        Ingredient ingredient = new Ingredient();
-                        ArrayList<IngredientModel> ingredientList = ingredient.userIngredient(1);
-                        Bitmap img2 = ingredient.getImg(ingredientList.get(0).img);
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                StringBuilder txt = new StringBuilder(result.description);
-                                txt.append("\n");
-                                for(int i=0;i<result.procedure.size();i++)
-                                    txt.append(Integer.toString(i)).append(". ").append("\n").append(result.procedure.get(i));
-                                test.setText(txt.toString());
-                                testImg.setImageBitmap(img);
-
-                                String txt2 = "";
-                                TextView text2 = (TextView) findViewById(R.id.recipeDetail_test2);
-                                ImageView testImg2 = (ImageView) findViewById(R.id.recipeDetail_testImg2);
-                                for(int i=0;i<ingredientList.size();i++)
-                                    txt2 += ingredientList.get(i).name + ", ";
-                                text2.setText(txt2);
-                                testImg2.setImageBitmap(img2);
-
-                            }
-                        });
-                    }
-                });
-                t.start();
-            }
-        });
     }
+
+    //기본 카메라앱 실행
     private void sendTakePhotoIntent() {
         try {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -120,6 +215,8 @@ public class RecipeDetailActivity extends AppCompatActivity {
             Log.d("imageRecognition",e.toString());
         }
     }
+
+    //기본 카메라앱에서 촬영한 사진 받아온 후 동작
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -135,16 +232,21 @@ public class RecipeDetailActivity extends AppCompatActivity {
                     byte[] bytes = baos.toByteArray();
                     String imgStr = Base64.encodeToString(bytes, Base64.DEFAULT);
                     JSONObject result = rs.PostImg("/imageRecognition",imgStr);
-                    if(result != null)
-                        Log.d("APItest",result.toString());
-                    else
+                    if(result != null) {
+                        try {
+                            String data = result.getString("data");
+                            if(data.equals("success"))
+                                Log.d("APItest", "success");
+                        }catch (Exception e){
+                            Log.d("APItest","fail");
+                        }
+                    }else
                         Log.d("APItest","fail");
                 }
             });
             t.start();
-            testImg.setImageBitmap(imageBitmap);
         }
         super.onActivityResult(requestCode, resultCode, data);
-    }
+    }*/
 
 }
