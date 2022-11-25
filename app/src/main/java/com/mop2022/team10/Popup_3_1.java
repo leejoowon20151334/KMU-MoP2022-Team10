f86be53f7ea8534e0c0477b7982c5d254cb56108
wjsquddn <54920381+wjsquddn@users.noreply.github.com>
2022-11-25 19:10:59 +0900

    수정본 입니다

--- app/src/main/java/com/mop2022/team10/Popup_3_1.java
+++ app/src/main/java/com/mop2022/team10/Popup_3_1.java
@@ -2,13 +2,16 @@ package com.mop2022.team10;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class Popup_3_1 extends Activity {
+    ImageView imageView_popup;
     TextView due_tv;
     TextView count_tv;
 
@@ -21,10 +24,16 @@ public class Popup_3_1 extends Activity {
         setContentView(R.layout.popup_3_1);
         due_tv = (TextView) findViewById(R.id.dialog_due);
         count_tv = (TextView) findViewById(R.id.dialog_count);
+        imageView_popup = (ImageView)findViewById(R.id.imageView_popup);
 
         Intent intent = getIntent();
+        Bitmap img = intent.getParcelableExtra("image");
         String data = intent.getStringExtra("data");
+        double count = intent.getDoubleExtra("count",0);
+
         due_tv.setText(data);
+        count_tv.setText(Double.toString(count));
+        imageView_popup.setImageBitmap(img);
     }
     public void mOnClose(View v){
         Intent intent = new Intent();
--- app/src/main/java/com/mop2022/team10/RecommendAdapter.java
+++ app/src/main/java/com/mop2022/team10/RecommendAdapter.java
@@ -1,6 +1,7 @@
 package com.mop2022.team10;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,14 +21,15 @@ public class RecommendAdapter extends RecyclerView.Adapter<RecommendAdapter.Reco
         ImageView recommendimageView;
         TextView recommendfood;
         TextView score;
-        RatingBar ratingBar;
+        TextView time;
+        TextView evaluate;
         RecommendViewHolder(View itemView){
             super(itemView);
             recommendimageView = itemView.findViewById(R.id.recomendimageView);
             recommendfood = itemView.findViewById(R.id.recomendfoodname);
             score = itemView.findViewById(R.id.score);
-            ratingBar = itemView.findViewById(R.id.ratingBar);
-
+            time = itemView.findViewById(R.id.time);
+            evaluate = itemView.findViewById(R.id.recipeDetail_evaluation6);
         }
     }
     @NonNull
@@ -42,10 +44,16 @@ public class RecommendAdapter extends RecyclerView.Adapter<RecommendAdapter.Reco
 
     @Override
     public void onBindViewHolder(RecommendViewHolder holder, int position) {
-        holder.recommendimageView.setImageResource(myDataList1.get(position).getImageResourceID());
+        holder.recommendimageView.setImageBitmap(myDataList1.get(position).getImageResource());
         holder.recommendfood.setText(myDataList1.get(position).getFoodName());
-        holder.ratingBar.setRating(myDataList1.get(position).getRate());
-        holder.score.setText("난이도 : "+Float.toString(myDataList1.get(position).getRate()));
+        if(myDataList1.get(position).getRate()<3){holder.score.setText("난이도:쉬워요");}
+        else if(myDataList1.get(position).getRate()<5){holder.score.setText("난이도:약간 쉬워요");}
+        else if(myDataList1.get(position).getRate()<7){holder.score.setText("난이도:할만해요");}
+        else if(myDataList1.get(position).getRate()<9){holder.score.setText("난이도:조금어려워요");}
+        else{holder.score.setText("난이도:어려워요");}
+        holder.time.setText(Integer.toString(myDataList1.get(position).getTime())+"분");
+        holder.evaluate.setText(Float.toString(myDataList1.get(position).getEvaluate()));
+//        holder.score.setText("난이도 : "+Float.toString(myDataList1.get(position).getRate()));
     }
 
     @Override
